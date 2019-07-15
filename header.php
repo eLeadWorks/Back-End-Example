81fbfa58655598d36dc79dab847b520100545ed6
eLeadWorks <42094056+eLeadWorks@users.noreply.github.com>
2019-07-15 02:11:55 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php the_title(); ?> - <?php bloginfo( ‘name’ ); ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<?php wp_head();?>
+  </head>
+  <body>
+    <div id="bg"></div>
+  <header>
+    <div id="title"><?php bloginfo( ‘name’ ); ?> - <?php bloginfo( 'description' ); ?></div>     
+  </header>
