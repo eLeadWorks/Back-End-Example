197b93890b4f0f4f3c20c6b7b4f510e5ccd37dd5
Asher <42094056+eLeadWorks@users.noreply.github.com>
2019-07-17 04:00:37 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,24 +2,15 @@
 /**
  * The template for displaying 404 pages (not found)
  **/
-
 get_header();
 ?>
 
-<section id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
-				</header>
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
-					<?php get_search_form(); ?>
-				</div>
-			</div>
-		</main>
-	</section>
-
+  <div class="error-page">
+    <h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'exampletheme' ); ?></h1>
+	<p><?php _e( 'It looks like nothing was found at this location. Are you an Employer here to view my coding examples?', 'exampletheme' ); ?></p>
+	<p><a href="https://eleadworks.com/backend-example/"><span style="font-size:24px;color:#fff;text-declaration:none;">Click here to view my code examples!</span></a></p> 
+  </div>
+  
 <?php
 get_footer();
 ?>
