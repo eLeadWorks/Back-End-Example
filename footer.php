fd8f0e2f8d6370c86b6ab08effc3e68798ba9919
Asher <42094056+eLeadWorks@users.noreply.github.com>
2019-07-17 04:14:00 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
     <div class="last-modified">Document last modified on <span id="last_modified"></span> .</div>
 </footer>
 
+
+<!-- i know not to load js from the footer although my functions.php currently isnt enqueing the script, ill debug later -->
 <script>
         var acc = document.getElementsByClassName("accordion");
         var i;
